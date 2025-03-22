3ca130b3a7a90f08047c0343ab8b173ab26f65ca
Wen-Jay <109235797+Wen-Jey@users.noreply.github.com>
2025-03-22 14:42:11 +0800

    管理系统（项目实战）
    go+gin+gorm+redis+mysql+vue项目实战

---
+++ 项目实战/admin-go-api/api/controller/captcha.go
@@ -0,0 +1,19 @@
+// 验证码 控制层
+
+package controller
+
+import (
+	"admin-api/api/service"
+	"admin-api/common/result"
+	"github.com/gin-gonic/gin"
+)
+
+// @Summary 验证码接口
+// @Produce json
+// @Description 验证码接口
+// @Success 200 {object} result.Result
+// @router /api/captcha [get]
+func Captcha(c *gin.Context) {
+	id, base64Image := service.CaptMake()
+	result.Success(c, map[string]interface{}{"idKey": id, "image": base64Image})
+}
---
+++ 项目实战/admin-go-vue/src/permission/index.js
@@ -0,0 +1,17 @@
+/**
+ * vue使用配置
+ */
+import authority from "./Authority"
+import Vue from "vue"
+
+const install = function (Vue) {
+    Vue.directive('authority', authority)
+}
+
+if (window.Vue) {
+    window.authority = authority
+    Vue.use(install)
+}
+
+authority.install = install
+export default authority
